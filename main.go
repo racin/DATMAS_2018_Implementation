7652466f55ed69ca8e352b6fef460090151e9fee
Racin Nygaard <racin.nygaard@gmail.com>
2018-03-24 01:00:15 +0100

    Hash

--- main.go
+++ main.go
@@ -18,8 +18,11 @@ import (
 	"github.com/tendermint/abci/types"
 	cmn "github.com/tendermint/tmlibs/common"
 	"github.com/tendermint/abci/server"
-	"github.com/tendermint/tmlibs/common"
+	//"github.com/tendermint/tmlibs/common"
 	"github.com/tendermint/tmlibs/log"
+	"github.com/multiformats/go-multihash"
+	"encoding/hex"
+	"crypto/sha1"
 )
 
 func NewServer(protoAddr, transport string, app types.Application) (cmn.Service, error) {
@@ -60,9 +63,39 @@ func main(){
 	}
 	fmt.Println("Racin har started en app! Transport: " + *abciPtr);
 	fmt.Println("Info om app: " + app.Info(types.RequestInfo{Version: "123"}).Data)
+/*	buf, _ := hex.DecodeString("48656c6c6f20476f7068657221")
+	fmt.Printf("%s\n", buf)
+	mHashBuf, _ := multihash.Encode([]byte("multihash"), multihash.SHA2_256)
+	mh, _ := multihash.Cast(mHashBuf);
+	fmt.Printf("hex: %s\n", hex.EncodeToString(mHashBuf))
+	fmt.Println(mh.B58String())
+	mHash, _ := multihash.Decode(mHashBuf)
+	sha256hex := hex.EncodeToString(mHash.Digest)
+	fmt.Printf("obj: %v 0x%x %d %s\n", mHash.Name, mHash.Code, mHash.Length, sha256hex)
+	fmt.Println()*/
+	// ignores errors for simplicity.
+	// don't do that at home.
+	// Decode a SHA1 hash to a binary buffer
+	//buf, _ := hex.DecodeString("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33")
+	buf:= []byte("multihash")//hex.DecodeString("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33")
+	// Create a new multihash with it.
+	fmt.Printf("%x\n", sha1.Sum(buf))
+	z := []byte(sha1.Sum(buf))
+	fmt.Println(hex.EncodeToString())
+	mHashBuf, _ := multihash.EncodeName(buf, "sha1")
+	// Print the multihash as hex string
+	fmt.Printf("hex: %s\n", hex.EncodeToString(mHashBuf))
+
+	// Parse the binary multihash to a DecodedMultihash
+	mHash, _ := multihash.Decode(mHashBuf)
+	// Convert the sha1 value to hex string
+	sha1hex := hex.EncodeToString(mHash.Digest)
+	// Print all the information in the multihash
+	fmt.Printf("obj: %v 0x%x %d %s\n", mHash.Name, mHash.Code, mHash.Length, sha1hex)
+	fmt.Println("Wait!")
 	// Wait forever
-	common.TrapSignal(func() {
+	/*common.TrapSignal(func() {
 		// Cleanup
 		srv.Stop()
-	})
+	})*/
 }
