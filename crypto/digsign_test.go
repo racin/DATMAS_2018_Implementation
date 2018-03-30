ed8c6d2838e33e24bcdbb25f8e3d805f0b593cdf
Racin Nygaard <racin.nygaard@gmail.com>
2018-03-30 14:52:47 +0200

    Signature

--- crypto/digsign_test.go
+++ crypto/digsign_test.go
@@ -3,13 +3,30 @@ package crypto
 import (
 	"crypto/rsa"
 	"crypto/rand"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"fmt"
 )
 
-func TestSignature(t *testing.T){
+func TestSignature(t *testing.T) {
 	// Generate a set of random keys
-	a, _ := rsa.GenerateKey(rand.Reader, 2048)
-	key := &Keys{private: a, public: &a.PublicKey}
+	keypair, _ := rsa.GenerateKey(rand.Reader, 2048)
+	key := &Keys{private: keypair, public: &keypair.PublicKey}
 
-	
+	data := []byte("Test data to sign and verify")
+	hashData, err := IPFSHashData(data)
+	if err != nil {
+		t.Fatal("Could not hash data")
+	}
+
+
+	fmt.Printf("Keys: %+v\n", keypair)
+	fmt.Printf("Key: %+v\n", key)
+	fmt.Printf("Data: %s\n", hashData)
+	signature := key.Sign(hashData)
+	fmt.Printf("Sign: %s\n", signature)
+	verify := key.Verify(hashData, signature)
+
+
+	assert.True(t, verify, "Signature did not match.")
 }
